fad2cde4c0951c8acbc01fcb08c122c26346a22f
giachi32-a11y <giachi32@gmail.com>
2026-05-14 17:35:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -253,7 +253,7 @@ export default function App() {
                 <a href="https://www.instagram.com/donblendzbarbershop?igsh=N3oxaTRnY3Z4bGk1" target="_blank" rel="noopener noreferrer">
                   <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" alt="Instagram" style={{ width: '32px', height: '32px' }} />
                 </a>
-                <a href="https://www.tiktok.com/@donblendz" target="_blank" rel="noopener noreferrer">
+                <a href="tiktok.com/@donblendzbarbershop?_r=1&_t=ZN-93BcAvmppAM" target="_blank" rel="noopener noreferrer">
                   <svg width="32" height="32" viewBox="0 0 448 512" fill="#fff" xmlns="http://www.w3.org/2000/svg">
       <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/>
     </svg>
