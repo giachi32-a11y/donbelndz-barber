d8d7703f4c073e63f832ab0c26fa1581f301cbf6
giachi32-a11y <giachi32@gmail.com>
2026-09-11 15:46:44 +0200

    Update StaffDashboard.jsx

--- src/StaffDashboard.jsx
+++ src/StaffDashboard.jsx
@@ -8,7 +8,7 @@ const THEME = {
   glass: 'rgba(255, 255, 255, 0.05)'
 };
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby0XXztoEzfU6b4t07EcyZnjglCwxsu_FoY1AOHiY2K1N4sC_-hI1yTphBGWWQUMp-Keg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwlmAZcs8sCrJjMqhsobFQ8gmPhfzSW0oM1OnoQFq7bZ2M1yTPhrV2oe3rLjypAZElHzw/exec";
 
 export default function StaffDashboard({ onBack }) {
   const [isAdmin, setIsAdmin] = useState(false);
