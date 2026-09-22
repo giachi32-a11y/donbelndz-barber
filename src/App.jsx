c4c6efb858f703eb0a3099d0998e1b110330eb37
giachi32-a11y <giachi32@gmail.com>
2026-09-22 16:12:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const THEME = {
   radius: '16px'
 };
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzGm2z6ZNSIB5__Cce0V7j3o5kwEXJTzjb7RCTbJsZMcs-FUwc_uT0S_BJaWSriM-RgZQ/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzf_IHMdBxPWYCB9PvH3s4gnhfJw46PzaAN2h-LkGd79IlmUdA-NwM7YBRTyfQZMItdsw/exec";
 
 const styles = {
   splash: { position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: '#000', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', zIndex: 9999, transition: 'opacity 0.8s ease' },
